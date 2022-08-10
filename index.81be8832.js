5b1687d7b3bf74e482e8254da11b04e69335131f
anatoliyshulika <shulika.anatoliy2@gmail.com>
2022-08-10 08:40:27 +0000

    Deploying to gh-pages from @ anatoliyshulika/icecream@347db1cd31a3b641dd082db3056dbf8e5f835da8 🚀

---
+++ index.81be8832.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slider").slick({arrows:!1,dots:!0,adaptiveHeight:!0,draggable:!1,easing:"250ms cubic-bezier(0.4, 0, 0.2, 1);"})}));
+//# sourceMappingURL=index.81be8832.js.map
--- index.da2ec61b.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".slider").slick({arrows:!1,dots:!0,adaptiveHeight:!0})}));
-//# sourceMappingURL=index.da2ec61b.js.map
