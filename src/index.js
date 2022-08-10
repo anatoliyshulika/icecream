9effcb2b5bf961de077981bb8fb8c1bdc6988f8e
Anatoliy Shulika <shulika.anatoliy2@gmail.com>
2022-08-10 20:36:07 +0300

    add media hero

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './js/counter';
-// import './js/gallery';
 import './js/mobile-menu';
 import './js/modal';
 import './js/scroll';
 import './js/scrolltoup';
 import './js/slick.min';
 import './js/slider';
+import './js/menu';
