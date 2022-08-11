3eab515b46a57c28f0a7a35cf88c1ba24bae1b4a
andreysulymka <andreysulymka5@gmail.com>
2022-08-11 15:49:57 +0300

    delete arrows

--- src/js/slider.js
+++ src/js/slider.js
@@ -9,9 +9,10 @@
 // });
 
 $('.slider').slick({
+  arrows: false,
   dots: true,
-  // infinite: true,
-  // speed: 300,
   slidesToShow: 1,
   adaptiveHeight: true,
+  draggable: false,
+  easing: '250ms cubic-bezier(0.4, 0, 0.2, 1);'
 });
