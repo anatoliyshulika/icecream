c9928b5e95b6b8d225f2123b86e356e57c2296fe
Anatoliy Shulika <shulika.anatoliy2@gmail.com>
2022-08-11 13:46:31 +0300

    Fix slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,9 +1,17 @@
-$(document).ready(function () {
-  $('.slider').slick({
-    arrows: false,
-    dots: true,
-    adaptiveHeight: true,
-    draggable: false,
-    easing: '250ms cubic-bezier(0.4, 0, 0.2, 1);',
-  });
+// $(document).ready(function () {
+//   $('.slider').slick({
+//     arrows: false,
+//     dots: true,
+//     adaptiveHeight: true,
+//     draggable: false,
+//     easing: '250ms cubic-bezier(0.4, 0, 0.2, 1);',
+//   });
+// });
+
+$('.slider').slick({
+  dots: true,
+  // infinite: true,
+  // speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
 });
