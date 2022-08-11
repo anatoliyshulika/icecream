95cdcb350716908a3c0d0de8c7f9e57de64a50f8
Anatoliy Shulika <shulika.anatoliy2@gmail.com>
2022-08-11 17:46:45 +0300

    Add scroll top

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,84 +1,81 @@
-(() => {
-  const menuBtnRef = document.querySelector('[data-menu-button]');
-  const mobileMenuRef = document.querySelector('[data-menu]');
-    const mobileOpen = document.querySelector('[data-open]');
-  const menuBtn = document.querySelector('[data-btn-button]');
-  const menuBtnAbout = document.querySelector('[data-about-button]');
-  const menuBtnHow = document.querySelector('[data-how-button]');
-  const menuBtnOur = document.querySelector('[data-our-button]');
-  const menuBtnContact = document.querySelector('[data-contact-button]');
+// (() => {
+//   const menuBtnRef = document.querySelector('[data-menu-button]');
+//   const mobileMenuRef = document.querySelector('[data-menu]');
+//     const mobileOpen = document.querySelector('[data-open]');
+//   const menuBtn = document.querySelector('[data-btn-button]');
+//   const menuBtnAbout = document.querySelector('[data-about-button]');
+//   const menuBtnHow = document.querySelector('[data-how-button]');
+//   const menuBtnOur = document.querySelector('[data-our-button]');
+//   const menuBtnContact = document.querySelector('[data-contact-button]');
 
+//   menuBtnRef.addEventListener('click', () => {
+//     const expanded =
+//       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
+//     menuBtnRef.classList.toggle('is-open');
+//     menuBtnRef.setAttribute('aria-expanded', !expanded);
 
+//     mobileMenuRef.classList.toggle('is-open');
 
-  menuBtnRef.addEventListener('click', () => {
-    const expanded =
-      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+//     mobileOpen.classList.toggle('menu-open');
+//   });
 
-    menuBtnRef.classList.toggle('is-open');
-    menuBtnRef.setAttribute('aria-expanded', !expanded);
+//   menuBtnAbout.addEventListener('click', () => {
+//     const expanded =
+//       menuBtnAbout.getAttribute('aria-expanded') === 'true' || false;
 
-    mobileMenuRef.classList.toggle('is-open');
+//     menuBtnAbout.classList.toggle('is-open');
+//     menuBtnAbout.setAttribute('aria-expanded', !expanded);
 
-    mobileOpen.classList.toggle('menu-open');
-  });
+//     mobileMenuRef.classList.toggle('is-open');
 
-  menuBtnAbout.addEventListener('click', () => {
-    const expanded =
-      menuBtnAbout.getAttribute('aria-expanded') === 'true' || false;
+//     mobileOpen.classList.toggle('menu-open');
+//   });
 
-    menuBtnAbout.classList.toggle('is-open');
-    menuBtnAbout.setAttribute('aria-expanded', !expanded);
+//   menuBtn.addEventListener('click', () => {
+//     const expanded = menuBtn.getAttribute('aria-expanded') === 'true' || false;
 
-    mobileMenuRef.classList.toggle('is-open');
+//     menuBtn.classList.toggle('is-open');
+//     menuBtn.setAttribute('aria-expanded', !expanded);
 
-    mobileOpen.classList.toggle('menu-open');
-  });
+//     mobileMenuRef.classList.toggle('is-open');
 
-  menuBtn.addEventListener('click', () => {
-    const expanded = menuBtn.getAttribute('aria-expanded') === 'true' || false;
+//     mobileOpen.classList.toggle('menu-open');
+//   });
 
-    menuBtn.classList.toggle('is-open');
-    menuBtn.setAttribute('aria-expanded', !expanded);
+//   menuBtnHow.addEventListener('click', () => {
+//     const expanded =
+//       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
-    mobileMenuRef.classList.toggle('is-open');
+//     menuBtnHow.classList.toggle('is-open');
+//     menuBtnHow.setAttribute('aria-expanded', !expanded);
 
-    mobileOpen.classList.toggle('menu-open');
-  });
+//     mobileMenuRef.classList.toggle('is-open');
 
-  menuBtnHow.addEventListener('click', () => {
-    const expanded =
-      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+//     mobileOpen.classList.toggle('menu-open');
+//   });
 
-    menuBtnHow.classList.toggle('is-open');
-    menuBtnHow.setAttribute('aria-expanded', !expanded);
+//   menuBtnOur.addEventListener('click', () => {
+//     const expanded =
+//       menuBtnOur.getAttribute('aria-expanded') === 'true' || false;
 
-    mobileMenuRef.classList.toggle('is-open');
+//     menuBtnOur.classList.toggle('is-open');
+//     menuBtnOur.setAttribute('aria-expanded', !expanded);
 
-    mobileOpen.classList.toggle('menu-open');
-  });
+//     mobileMenuRef.classList.toggle('is-open');
 
-  menuBtnOur.addEventListener('click', () => {
-    const expanded =
-      menuBtnOur.getAttribute('aria-expanded') === 'true' || false;
+//     mobileOpen.classList.toggle('menu-open');
+//   });
 
-    menuBtnOur.classList.toggle('is-open');
-    menuBtnOur.setAttribute('aria-expanded', !expanded);
+//   menuBtnContact.addEventListener('click', () => {
+//     const expanded =
+//       menuBtnContact.getAttribute('aria-expanded') === 'true' || false;
 
-    mobileMenuRef.classList.toggle('is-open');
+//     menuBtnContact.classList.toggle('is-open');
+//     menuBtnContact.setAttribute('aria-expanded', !expanded);
 
-    mobileOpen.classList.toggle('menu-open');
-  });
+//     mobileMenuRef.classList.toggle('is-open');
 
-  menuBtnContact.addEventListener('click', () => {
-    const expanded =
-      menuBtnContact.getAttribute('aria-expanded') === 'true' || false;
-
-    menuBtnContact.classList.toggle('is-open');
-    menuBtnContact.setAttribute('aria-expanded', !expanded);
-
-    mobileMenuRef.classList.toggle('is-open');
-
-    mobileOpen.classList.toggle('menu-open');
-  });
-})();
+//     mobileOpen.classList.toggle('menu-open');
+//   });
+// })();
